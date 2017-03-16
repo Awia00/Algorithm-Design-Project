af3c89fd1b3fc11bccea548f98f484855dbe4552
Mikael Lindemann Jepsen <mikaellindemannjepsen@gmail.com>
2017-03-16 11:08:38 +0100

    Read from file, or stdin

--- PACE/Tools/src/PaceToDot.java
+++ PACE/Tools/src/PaceToDot.java
@@ -1,16 +1,10 @@
-import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
 
 public class PaceToDot {
 
     public static void main(String[] args) throws IOException {
-        if (args.length == 0) {
-            System.out.println("Usage: java PaceToDot <filename>");
-            return;
-        }
-
-        try (Scanner scanner = new Scanner(new File(args[0]))) {
+        try (Scanner scanner = new Scanner(Util.getInput(args))) {
             System.out.println("Graph G {");
             while (scanner.hasNextLine()) {
                 String line = scanner.nextLine();
