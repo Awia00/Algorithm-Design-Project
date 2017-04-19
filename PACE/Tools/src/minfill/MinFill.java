837755d4fe14090c99dd4b8e77067b5516874f53
Anders Wind <aws@netcompany.com>
2017-04-19 15:22:17 +0200

    Changed the order of the methods to make sense

--- PACE/Tools/src/minfill/MinFill.java
+++ PACE/Tools/src/minfill/MinFill.java
@@ -34,145 +34,6 @@ public class MinFill {
         return Optional.empty();
     }
 
-    @Contract(pure = true)
-    public Optional<Graph> stepB2(Graph g, int k) {
-        System.err.printf("Step B2: Non-reducible instance found. k=%d\n", k);
-        Set<Set<Integer>> piI = generateVitalPotentialMaximalCliques(g, k);
-
-        return stepC(g, k, piI);
-    }
-
-    @Contract(pure = true)
-    public Map<Pair, Set<Set<Integer>>> generatePiSC(Graph g, Set<Set<Integer>> piI)
-    {
-        Map<Pair, Set<Set<Integer>>> piSC = new HashMap<>();
-        for (Set<Integer> omega : piI) {
-            Graph gMinusOmega = g.inducedBy(g.vertices().minus(omega));
-            Set<Set<Integer>> components = gMinusOmega.components();
-            for (Set<Integer> component : components) {
-                Set<Integer> s = g.neighborhood(component);
-                for (Set<Integer> c : g.fullComponents(s)) {
-                    if(s.isProperSubsetOf(omega) && omega.isSubsetOf(s.union(c)))
-                    {
-                        Pair pair = new Pair<>(s, c);
-                        if(!piSC.containsKey(pair)) piSC.put(pair, new ImmutableSet<>(omega));
-                        else piSC.put(pair, piSC.get(pair).add(omega));
-                    }
-                }
-            }
-        }
-        return piSC;
-    }
-
-    public Set<Edge> minFillF(Graph f, Pair sc, Map<Pair, Set<Set<Integer>>> piSC, Map<Graph, Set<Edge>> memoizer){
-        Set<Edge> memoizedResult = memoizer.get(f);
-        if(memoizedResult != null) {
-            memoizerHits.increment();
-            return memoizedResult;
-        }
-
-        Set<Edge> result = f.getNonEdges();
-        for (Set<Integer> omegaPrime : piSC.get(sc)) {
-            Set<Edge> fill = f.cliqueify(omegaPrime);
-            Graph filled = f.addEdges(fill);
-
-            for (Set<Integer> cPrime : f.inducedBy(f.vertices().minus(omegaPrime)).components()) {
-                if (fill.size() >= result.size()) break;
-                Set<Integer> neighborhoodCPrime = f.neighborhood(cPrime);
-                fill = fill.union(minFillF(filled.inducedBy(cPrime.union(neighborhoodCPrime)), new Pair<>(neighborhoodCPrime, cPrime), piSC, memoizer));
-            }
-            if (fill.size() < result.size()) result = fill;
-        }
-
-        memoizer.put(f, result);
-        return result;
-    }
-
-    @Contract(pure = true)
-    public Optional<Graph> stepC(Graph g, int k, Set<Set<Integer>> piI) {
-        System.err.println("Step C: All vital potential maximal cliques found.");
-        Map<Pair, Set<Set<Integer>>> piSC = generatePiSC(g, piI);
-        Map<Graph, Set<Edge>> memoizer = new HashMap<>();
-        for (Set<Integer> omega : piI) {
-            Set<Edge> fill = g.cliqueify(omega);
-            Graph filled = g.addEdges(fill);
-            for (Set<Integer> c : g.inducedBy(g.vertices().minus(omega)).components()) {
-                Set<Integer> neighborhoodC = g.neighborhood(c);
-                fill = fill.union(minFillF(filled.inducedBy(c.union(neighborhoodC)), new Pair<>(neighborhoodC, c), piSC, memoizer));
-            }
-            if(fill.size()<=k) return Optional.of(g.addEdges(fill));
-        }
-        return Optional.empty();
-    }
-
-    // Implementation of Lemma 4.1
-    // TODO: Check that we branch correctly on component size.
-    @Contract(pure = true)
-    private Set<Set<Integer>> enumerateQuasiCliques(Graph g, int k) {
-        Set<Set<Integer>> potentialMaximalCliques = Set.empty();
-        Set<Set<Integer>> vertexSubsets = g.vertices().subsetsOfSizeAtMost((int)(5*Math.sqrt(k)));
-
-        for (Set<Integer> z : vertexSubsets) {
-            Set<Integer> gMinusZ = g.vertices().minus(z);
-            Graph h = g.inducedBy(gMinusZ).minimalTriangulation();
-
-            // Case 1
-            for (Set<Integer> s : h.minimalSeparatorsOfChordalGraph()) {
-                if(g.inducedBy(s).isClique()){
-                    Set<Integer> c = s.union(z);
-                    if (g.isVitalPotentialMaximalClique(c, k)) {
-                        potentialMaximalCliques = potentialMaximalCliques.add(c);
-                    }
-                }
-            }
-
-            for (Set<Integer> maximalClique : h.maximalCliquesOfChordalGraph()) {
-                // Case 2
-                if (g.isClique(maximalClique)) {
-                    Set<Integer> c = maximalClique.union(z);
-                    if (g.isVitalPotentialMaximalClique(c, k)) {
-                        potentialMaximalCliques = potentialMaximalCliques.add(c);
-                    }
-                }
-
-                // Case 3
-                for (Integer y : z) {
-                    Set<Integer> Y = Set.empty();
-                    for (Set<Integer> bi : g.inducedBy(g.vertices().minus(z.union(maximalClique))).components()) {
-                        Y = Y.union(bi.add(y));
-                    }
-                    Set<Integer> c = g.neighborhood(Y).add(y);
-                    if (g.isVitalPotentialMaximalClique(c, k)) {
-                        potentialMaximalCliques = potentialMaximalCliques.add(c);
-                    }
-                }
-            }
-        }
-        return potentialMaximalCliques;
-    }
-
-    @Contract(pure = true)
-    public Set<Set<Integer>> generateVitalPotentialMaximalCliques(Graph g, int k) {
-        // enumerate quasi-cliques. (Step 1)
-        Set<Set<Integer>> potentialMaximalCliques = enumerateQuasiCliques(g, k);
-
-        // all vertex subsets of size at most 5*sqrt(k)+2 (step 2)
-        for (Set<Integer> vertices : g.vertices().subsetsOfSizeAtMost((int) (5 * Math.sqrt(k) + 2))) {
-            if (g.isVitalPotentialMaximalClique(vertices, k)) {
-                potentialMaximalCliques = potentialMaximalCliques.add(vertices);
-            }
-        }
-
-        // step 3 of generating vital potential maximal cliques
-        for (Integer vertex : g.vertices()) {
-            Set<Edge> fill = g.cliqueify(g.neighborhood(vertex));
-            Graph h = g.addEdges(fill);
-
-            potentialMaximalCliques = potentialMaximalCliques.union(enumerateQuasiCliques(h, k));
-        }
-        return potentialMaximalCliques;
-    }
-
     /**
      * @param g A graph.
      * @param k Max number of edges to make g chordal.
@@ -233,6 +94,145 @@ public class MinFill {
             }
         }
         return Set.of(changes);
+    }
+
+    @Contract(pure = true)
+    public Optional<Graph> stepB2(Graph g, int k) {
+        System.err.printf("Step B2: Non-reducible instance found. k=%d\n", k);
+        Set<Set<Integer>> piI = generateVitalPotentialMaximalCliques(g, k);
+
+        return stepC(g, k, piI);
+    }
+
+    @Contract(pure = true)
+    public Set<Set<Integer>> generateVitalPotentialMaximalCliques(Graph g, int k) {
+        // enumerate quasi-cliques. (Step 1)
+        Set<Set<Integer>> potentialMaximalCliques = enumerateQuasiCliques(g, k);
+
+        // all vertex subsets of size at most 5*sqrt(k)+2 (step 2)
+        for (Set<Integer> vertices : g.vertices().subsetsOfSizeAtMost((int) (5 * Math.sqrt(k) + 2))) {
+            if (g.isVitalPotentialMaximalClique(vertices, k)) {
+                potentialMaximalCliques = potentialMaximalCliques.add(vertices);
+            }
+        }
+
+        // step 3 of generating vital potential maximal cliques
+        for (Integer vertex : g.vertices()) {
+            Set<Edge> fill = g.cliqueify(g.neighborhood(vertex));
+            Graph h = g.addEdges(fill);
+
+            potentialMaximalCliques = potentialMaximalCliques.union(enumerateQuasiCliques(h, k));
+        }
+        return potentialMaximalCliques;
+    }
+
+    // Implementation of Lemma 4.1
+    // TODO: Check that we branch correctly on component size.
+    @Contract(pure = true)
+    private Set<Set<Integer>> enumerateQuasiCliques(Graph g, int k) {
+        Set<Set<Integer>> potentialMaximalCliques = Set.empty();
+        Set<Set<Integer>> vertexSubsets = g.vertices().subsetsOfSizeAtMost((int)(5*Math.sqrt(k)));
+
+        for (Set<Integer> z : vertexSubsets) {
+            Set<Integer> gMinusZ = g.vertices().minus(z);
+            Graph h = g.inducedBy(gMinusZ).minimalTriangulation();
+
+            // Case 1
+            for (Set<Integer> s : h.minimalSeparatorsOfChordalGraph()) {
+                if(g.inducedBy(s).isClique()){
+                    Set<Integer> c = s.union(z);
+                    if (g.isVitalPotentialMaximalClique(c, k)) {
+                        potentialMaximalCliques = potentialMaximalCliques.add(c);
+                    }
+                }
+            }
+
+            for (Set<Integer> maximalClique : h.maximalCliquesOfChordalGraph()) {
+                // Case 2
+                if (g.isClique(maximalClique)) {
+                    Set<Integer> c = maximalClique.union(z);
+                    if (g.isVitalPotentialMaximalClique(c, k)) {
+                        potentialMaximalCliques = potentialMaximalCliques.add(c);
+                    }
+                }
+
+                // Case 3
+                for (Integer y : z) {
+                    Set<Integer> Y = Set.empty();
+                    for (Set<Integer> bi : g.inducedBy(g.vertices().minus(z.union(maximalClique))).components()) {
+                        Y = Y.union(bi.add(y));
+                    }
+                    Set<Integer> c = g.neighborhood(Y).add(y);
+                    if (g.isVitalPotentialMaximalClique(c, k)) {
+                        potentialMaximalCliques = potentialMaximalCliques.add(c);
+                    }
+                }
+            }
+        }
+        return potentialMaximalCliques;
+    }
+
+    @Contract(pure = true)
+    public Optional<Graph> stepC(Graph g, int k, Set<Set<Integer>> piI) {
+        System.err.println("Step C: All vital potential maximal cliques found.");
+        Map<Pair, Set<Set<Integer>>> piSC = generatePiSC(g, piI);
+        Map<Graph, Set<Edge>> memoizer = new HashMap<>();
+        for (Set<Integer> omega : piI) {
+            Set<Edge> fill = g.cliqueify(omega);
+            Graph filled = g.addEdges(fill);
+            for (Set<Integer> c : g.inducedBy(g.vertices().minus(omega)).components()) {
+                Set<Integer> neighborhoodC = g.neighborhood(c);
+                fill = fill.union(minFillF(filled.inducedBy(c.union(neighborhoodC)), new Pair<>(neighborhoodC, c), piSC, memoizer));
+            }
+            if(fill.size()<=k) return Optional.of(g.addEdges(fill));
+        }
+        return Optional.empty();
+    }
+
+    @Contract(pure = true)
+    public Map<Pair, Set<Set<Integer>>> generatePiSC(Graph g, Set<Set<Integer>> piI)
+    {
+        Map<Pair, Set<Set<Integer>>> piSC = new HashMap<>();
+        for (Set<Integer> omega : piI) {
+            Graph gMinusOmega = g.inducedBy(g.vertices().minus(omega));
+            Set<Set<Integer>> components = gMinusOmega.components();
+            for (Set<Integer> component : components) {
+                Set<Integer> s = g.neighborhood(component);
+                for (Set<Integer> c : g.fullComponents(s)) {
+                    if(s.isProperSubsetOf(omega) && omega.isSubsetOf(s.union(c)))
+                    {
+                        Pair pair = new Pair<>(s, c);
+                        if(!piSC.containsKey(pair)) piSC.put(pair, new ImmutableSet<>(omega));
+                        else piSC.put(pair, piSC.get(pair).add(omega));
+                    }
+                }
+            }
+        }
+        return piSC;
+    }
+
+    public Set<Edge> minFillF(Graph f, Pair sc, Map<Pair, Set<Set<Integer>>> piSC, Map<Graph, Set<Edge>> memoizer){
+        Set<Edge> memoizedResult = memoizer.get(f);
+        if(memoizedResult != null) {
+            memoizerHits.increment();
+            return memoizedResult;
+        }
+
+        Set<Edge> result = f.getNonEdges();
+        for (Set<Integer> omegaPrime : piSC.get(sc)) {
+            Set<Edge> fill = f.cliqueify(omegaPrime);
+            Graph filled = f.addEdges(fill);
+
+            for (Set<Integer> cPrime : f.inducedBy(f.vertices().minus(omegaPrime)).components()) {
+                if (fill.size() >= result.size()) break;
+                Set<Integer> neighborhoodCPrime = f.neighborhood(cPrime);
+                fill = fill.union(minFillF(filled.inducedBy(cPrime.union(neighborhoodCPrime)), new Pair<>(neighborhoodCPrime, cPrime), piSC, memoizer));
+            }
+            if (fill.size() < result.size()) result = fill;
+        }
+
+        memoizer.put(f, result);
+        return result;
     }
 }
 
