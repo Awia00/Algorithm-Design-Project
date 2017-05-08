066b5cf743bcc95ab0b35a0b0904e15898a44a49
Anders Wind <aws@netcompany.com>
2017-05-08 19:44:48 +0200

    Can now print a graph in dot format

--- PACE/Tools/src/utils/PaceToDot.java
+++ PACE/Tools/src/utils/PaceToDot.java
@@ -1,21 +1,37 @@
 package utils;
 
+import minfill.graphs.Edge;
+import minfill.graphs.Graph;
+
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Scanner;
 
 public class PaceToDot {
 
     public static void main(String[] args) throws IOException {
-        try (Scanner scanner = new Scanner(Util.getInput(args))) {
-            System.out.println("Graph G {");
+        System.out.println(generateFromStream(Util.getInput(args)));
+    }
+
+    public static String generateFromStream(InputStream stream){
+        String result = "Graph g {";
+        try (Scanner scanner = new Scanner(stream)) {
             while (scanner.hasNextLine()) {
                 String line = scanner.nextLine();
 
                 String[] split = line.split(" ");
 
-                System.out.format("\t%s--%s\n", split[0], split[1]);
+                result += String.format("\t%s--%s\n", split[0], split[1]);
             }
-            System.out.println("}");
         }
+        return result + "}";
+    }
+
+    public static String generateFromGraph(Graph g){
+        String result = "Graph g {";
+        for (Edge edge : g.getEdges()) {
+            result += String.format("\t%s--%s\n", edge.from, edge.to);
+        }
+        return result + "}";
     }
 }
